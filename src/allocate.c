6523853beb1b8bf648c74904bc278a9d5992ef12
Thomas Woodruff <twoodruff@student.unimelb.edu.au>
2021-03-22 21:24:32 +1100

    Stored all processes in an array instead of reading them one-by-one.

--- src/allocate.c
+++ src/allocate.c
@@ -7,6 +7,7 @@
 #include "utils.h"
 
 #define MAX_PROCESSORS 1024
+#define INITIAL_PROCESSES_BUDGET 100
 
 
 /*
@@ -42,20 +43,22 @@ void parse_cli(int argc, char **argv, char **file_flag, int processor_flag) {
 }
 
 
-int main(int argc, char **argv) {
+/*
+Reads every line in the given file into a an array of processes.
+Does absolutely no format checking.
+The returned array should be in the same order as the file was.
+*/
+process **read_input_file(char *filename) {
 
-    // Parse and check command-line arguments.
-    char *input_file_name = NULL;
-    int number_of_processes = 0;
-    parse_cli(argc, argv, &input_file_name, number_of_processes);
-
-    FILE *input_process_file = fopen(input_file_name, "r");
+    FILE *input_process_file = fopen(filename, "r");
     assert(input_process_file);
 
-    int time = 0;
-    min_heap *processor = initialise_heap(100);
+    process **all_processes = malloc(sizeof(process*) * INITIAL_PROCESSES_BUDGET);
+    assert(all_processes);
 
-    // Run scheduling algorithm on input file, whilst reading it.
+    int index = 0;
+    int max_length = INITIAL_PROCESSES_BUDGET;
+
     int arrival_time;
     int pid;
     int run_time;
@@ -63,60 +66,66 @@ int main(int argc, char **argv) {
     while (fscanf(input_process_file, "%d %d %d %c", &arrival_time, &pid, &run_time, &parallelisable_char) == 4) {
         // printf("%d %d %d %c\n", arrival_time, pid, run_time, parallelisable_char);
         process *new_process = process_from_row(arrival_time, pid, run_time, parallelisable_char);
-
-        // Just do Task 1 to get your head around it.
-        int next_arrival_time = new_process->arrival_time;
-        while (time < next_arrival_time && is_empty(processor) != true) {
-            process *running_process = pop(&processor);
-            printf("%d,RUNNING,pid=%d,remaining_time=%d,cpu=%d\n", time, running_process->pid, running_process->remaining_run_time, 0);
-            running_process->remaining_run_time--;
-            if (running_process->remaining_run_time <= 0) {
-                printf("%d,FINISHED,pid=%d,proc_remaining=%d\n", time, running_process->pid, processor->last_index);
-                printf("Process finished: ");
-                print_process(running_process);
-                free(running_process);
-            } else {
-                push(&processor, running_process);
-            }
-            time++;
+        
+        // Allocate more memory if needed.
+        if (index > max_length) {
+            max_length *= 2;
+            all_processes = realloc(all_processes, sizeof(process*) * max_length);
+            assert(all_processes);
         }
-        time = next_arrival_time;  // Fast forward in case heap was empty a while ago.
-        push(&processor, new_process);
+
+        all_processes[index] = new_process;
+        index++;
     }
-
-    while (is_empty(processor) != true) {
-        process *running_process = pop(&processor);
-        running_process->remaining_run_time--;
-        if (running_process->remaining_run_time <= 0) {
-            printf("Process finished: ");
-            print_process(running_process);
-            free(running_process);
-        } else {
-            push(&processor, running_process);
-        }
-        time++;
-    }
-
-    /*
-    0 4 30 n
-    3 2 40 n
-    5 1 20 n
-    20 3 30 n
-
-    0,RUNNING,pid=4,remaining_time=30,cpu=0
-    5,RUNNING,pid=1,remaining_time=20,cpu=0
-    25,FINISHED,pid=1,proc_remaining=3
-    25,RUNNING,pid=4,remaining_time=25,cpu=0
-    50,FINISHED,pid=4,proc_remaining=2
-    50,RUNNING,pid=3,remaining_time=30,cpu=0
-    80,FINISHED,pid=3,proc_remaining=1
-    80,RUNNING,pid=2,remaining_time=40,cpu=0
-    120,FINISHED,pid=2,proc_remaining=0
-    Turnaround time 62
-    Time overhead 2.93 1.9
-    Makespan 120
-    */
-
     fclose(input_process_file);
+
+    // Set sentinel to NULL for when you iterate through the array later.
+    all_processes[index] = NULL;
+    return all_processes;
+}
+
+
+int main(int argc, char **argv) {
+
+    char *input_file_name = NULL;
+    int number_of_processors = 0;
+
+    // Parse and check command-line arguments.
+    parse_cli(argc, argv, &input_file_name, number_of_processors);
+
+    // Read input file into an array of processes. (Watch out for weird pointer problems caused by this array)
+    process **all_processes = read_input_file(input_file_name);
+
+    // Actual Algorithm
+    int time = 0;
+    min_heap *processor = initialise_heap(INITIAL_PROCESSES_BUDGET);
+
+
+
+
+
+    
+    free_min_heap(processor);
     return 0;
 }
+
+
+/*
+0 4 30 n
+3 2 40 n
+5 1 20 n
+20 3 30 n
+
+0,RUNNING,pid=4,remaining_time=30,cpu=0
+5,RUNNING,pid=1,remaining_time=20,cpu=0
+25,FINISHED,pid=1,proc_remaining=3
+25,RUNNING,pid=4,remaining_time=25,cpu=0
+50,FINISHED,pid=4,proc_remaining=2
+50,RUNNING,pid=3,remaining_time=30,cpu=0
+80,FINISHED,pid=3,proc_remaining=1
+80,RUNNING,pid=2,remaining_time=40,cpu=0
+120,FINISHED,pid=2,proc_remaining=0
+Turnaround time 62
+Time overhead 2.93 1.9
+Makespan 120
+*/
