969d3ad48bbf28b608b7f7b46aa4a8fef1059b13
Thomas Woodruff <twoodruff@student.unimelb.edu.au>
2021-03-27 11:13:57 +1100

    Cleaned up comments.

--- src/allocate.c
+++ src/allocate.c
@@ -9,8 +9,7 @@
 
 
 /*
-Parses and does value checking.
-For processor_flag, atoi will try it's best to deal with non-integer input.
+Parses command line input and does value checking.
 */
 void parse_cli(int argc, char **argv, char **file_flag, int *processor_flag) {
     int option;
@@ -43,8 +42,8 @@ void parse_cli(int argc, char **argv, char **file_flag, int *processor_flag) {
 
 /*
 Reads every line in the given file into an array of processes.
-Does absolutely no format checking.
 The returned array should be in the same order as the file was.
+Does not check input file formatting; malformed input will crash this program.
 */
 process **read_input_file(char *filename, int *number_of_processes) {
 
@@ -54,14 +53,14 @@ process **read_input_file(char *filename, int *number_of_processes) {
     process **all_processes = malloc(sizeof(process*) * INITIAL_PROCESSES_BUDGET);
     assert(all_processes);
 
-    int index = 0;
-    int max_length = INITIAL_PROCESSES_BUDGET;
-
-    int arrival_time;
     // if pid in file is longer than PID_MAX_LENGTH, the while loop below won't even run once.
+    int arrival_time;
     char pid[PID_MAX_LENGTH];
     int run_time;
     char parallelisable_char;
+
+    int index = 0;
+    int max_length = INITIAL_PROCESSES_BUDGET;
     while (fscanf(input_process_file, "%d %s %d %c", &arrival_time, pid, &run_time, &parallelisable_char) == 4) {
         // printf("%d %s %d %c\n", arrival_time, pid, run_time, parallelisable_char);
         process *new_process = process_from_row(arrival_time, pid, run_time, parallelisable_char);
@@ -89,23 +88,20 @@ process **read_input_file(char *filename, int *number_of_processes) {
 Go through each cpu and give each first process one second of cpu time.
 - Check for and print finished processes first.
 - Check for and print running processes second.
+- Works by kind of using message passing between the for loops.
+This function would be a lot nicer if I didn't have to print out everything 
+in such a ridiculously specific order.
 */
 void run_one_second(multicore **cores, int *remaining_processes, int *time, int *total_turnaround, double *max_overhead, double *average_overhead) {
 
     // Sort cpu's on just cpu id.
-    // Have to do this to print stuff out as the spec requested.
     multicore_heapify(cores, true);
     multicore_sort(cores, true);    
 
     cpu *current_cpu = NULL;
     process *first_process = NULL;
 
-    // check if remaining processes as changed.
-    // This is the most ridiculous assigment
-    // I could have learnt far more important shit in
-    // the time it's taken me to write all of these ridiculous
-    // functions just to satisfy all of your stupid edge cases
-    // oh and I still know fuck all about scheduling, cause i haven't actually had time to look that up.
+    // Check how many processes will finish in this second
     for (int j = (*cores)->last_index; j >= 1; j--) {
         current_cpu = ((*cores)->cpu_array)[j];
         if (cpu_is_empty(current_cpu) == true) {
@@ -119,7 +115,7 @@ void run_one_second(multicore **cores, int *remaining_processes, int *time, int 
         }
     }
 
-    // Print finished processes first
+    // Check for and print finished processes first
     for (int j = (*cores)->last_index; j >= 1; j--) {
         current_cpu = ((*cores)->cpu_array)[j];
 
@@ -132,6 +128,10 @@ void run_one_second(multicore **cores, int *remaining_processes, int *time, int 
         // Check if process has finished.
         if (first_process->remaining_run_time == 0) {
             // TODO: edit this to handle sub-processes.
+
+            if (first_process->is_sub_process) {
+                // c'mon
+            }
 
             // Calculate statistics
             int turnaround_time = *time - first_process->arrival_time; 
@@ -147,7 +147,7 @@ void run_one_second(multicore **cores, int *remaining_processes, int *time, int 
         }
     }
 
-    // Print running processes second
+    // Check for and print running processes.
     for (int j = (*cores)->last_index; j >= 1; j--) {
         current_cpu = ((*cores)->cpu_array)[j];
         first_process = cpu_peek(current_cpu);
@@ -189,6 +189,30 @@ int main(int argc, char **argv) {
 
     /*
     -----------------------Actual Algorithm-----------------------
+    This program uses three types of structs:
+    - process
+    - cpu
+    - multicore
+
+    cpu and multicore both wrap around min heaps, and are sorted differently.
+    - cpu
+        contains processes
+        sorts those processes on:
+            1. remaining run-time
+            2. process id
+    -multicore
+        contains cpu's
+        sorts those cpu's on:
+            1. total remaining run-time
+            2. cpu id
+
+    All of the processes that 'arrive' in one second are added to a temporary 
+    cpu (which no processes will run on). Every second, all of the processes on that cpu
+    are popped off, and added to the actual multicore in order of least run-time.
+
+    Every second, the multicore goes through every cpu (in order of cpu id) and runs
+    each cpu for one second, until there are no more processes left to add, at which point
+    it just runs all the existing processes to completion.
     */
 
     int time = 0;
@@ -198,7 +222,7 @@ int main(int argc, char **argv) {
     // process **parallelized;
     process **all_processes = read_input_file(input_file_name, &number_of_processes);
     multicore *cores = initialise_cores(number_of_processors);
-    cpu *temp_process_heap = initialise_cpu(INITIAL_PROCESSES_BUDGET, -1);
+    cpu *temp_process_buffer = initialise_cpu(INITIAL_PROCESSES_BUDGET, -1);
 
     int i = 0;
     while (true) {
@@ -207,7 +231,7 @@ int main(int argc, char **argv) {
         current_process = all_processes[i];
         next_process = all_processes[i + 1];
         if (current_process != NULL) {
-            cpu_push(&temp_process_heap, current_process);
+            cpu_push(&temp_process_buffer, current_process);
             i++;
         }
 
@@ -215,17 +239,16 @@ int main(int argc, char **argv) {
         while (next_process != NULL && next_process->arrival_time == current_process->arrival_time) {
             current_process = all_processes[i];
             next_process = all_processes[i + 1];
-            cpu_push(&temp_process_heap, current_process);
+            cpu_push(&temp_process_buffer, current_process);
             i++;
         }
         
-        // Now go through all of the processes that have arrived in the last second and
-        // add them to cpu(s).
-        while (cpu_is_empty(temp_process_heap) != true) {
+        // Now add all of the processes that have arrived in the last second to cpu(s).
+        while (cpu_is_empty(temp_process_buffer) != true) {
 
-            // Watch out for this little fucker creating memory problems
-            process *process_to_add = cpu_pop(&temp_process_heap);
-            assert(process_to_add);
+            // Watch out for this little fucker creating memory problems TODO
+            process *process_to_add = cpu_pop(&temp_process_buffer);
+            assert(process_to_add);  // TODO: remove
 
             if (process_to_add->is_parallelisable == true) {
                 /*
@@ -257,7 +280,6 @@ int main(int argc, char **argv) {
             break;
 
         } else while (time < next_process->arrival_time) {
-            // Ensure processes with same arrival time all get added.
             // Run all the processes up till the next arrival time.
             run_one_second(&cores, &remaining_processes, &time, &total_turnaround, &max_overhead, &total_overhead);
         }
@@ -266,7 +288,7 @@ int main(int argc, char **argv) {
     printf("Turnaround time %d\n", divide_round_int(total_turnaround, number_of_processes));
     printf("Time overhead %g %g\n", round_double_to_2(max_overhead), round_double_to_2(total_overhead / number_of_processes));
     printf("Makespan %d\n", time - 1);
-    free_cpu(temp_process_heap);
+    free_cpu(temp_process_buffer);
     free_cores(cores);
     free(all_processes);
     return 0;
